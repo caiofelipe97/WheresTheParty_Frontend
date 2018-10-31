3c0a3d625c938ab7b6877e40f74e9573a8b48380
Caio Melo <caiofelipee17@gmail.com>
2018-10-31 15:21:43 -0300

    Add route and house component

--- wheres_the_party/src/App.js
+++ wheres_the_party/src/App.js
@@ -1,14 +1,22 @@
 import React, { Component } from 'react';
 import HouseList from './components/mainScreen/HouseList';
+import House from './components/houseScreen/House';
 import Header from './components/Header';
+import { BrowserRouter as Router, Route} from 'react-router-dom';
+
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-      <Header/>
-        <HouseList></HouseList>
+      <Router>
+        <div>
+          <Header/>
+          <Route exact={true} path="/" component={HouseList}/>
+          <Route path="/house/:houseId" component={House}/>
+        </div>
+      </Router>
       </div>
     );
   }
